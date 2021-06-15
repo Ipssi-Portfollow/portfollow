36ca9234c16bfded0d8a12b437492f487ba15298
Ryan Daoud <ryan.daoud07@gmail.com>
2021-06-15 17:21:41 +0200

    Feat:Add User Profile Design

--- portfollow-app/migrations/Version20210615092347.php
+++ portfollow-app/migrations/Version20210615092347.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210614101349 extends AbstractMigration
+final class Version20210615092347 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,14 +20,14 @@ final class Version20210614101349 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_8D93D6495126AC48 ON user');
-        $this->addSql('ALTER TABLE user CHANGE mail email VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE user CHANGE pseudo pseudo VARCHAR(200) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user CHANGE email mail VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6495126AC48 ON user (mail)');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('ALTER TABLE user CHANGE pseudo pseudo VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
